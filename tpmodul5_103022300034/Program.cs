5be23884150a703a9024b8ead9b7095ca235ef1c
Ariq Hisyam Nabil <ariqhisyam62@gmail.com>
2025-03-16 15:15:44 +0700

    Menambahkan Class Data Generic

--- tpmodul5_103022300034/Program.cs
+++ tpmodul5_103022300034/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}!");
+    }
+}
+
+class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Ariq");
+
+        DataGeneric<string> data = new DataGeneric<string>("103022300034");
+        data.PrintData();
+    }
+}
